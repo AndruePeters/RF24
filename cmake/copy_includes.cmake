a3e7b9c6b51ff4ded3cd4847a26e796bde17f955
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2021-04-26 11:07:08 -0400

    cmake function to copy config file and add to target

--- cmake/copy_includes.cmake
+++ cmake/copy_includes.cmake
@@ -1,5 +1,7 @@
-# let arg1 be the target name
-# arg2 be the original file
+# Copies an includes file from one of the utility drivers and then adds it to target
+#
+# target - cmake target that has this file added in its target_include_directories
+# source - path to source file for the original includes
 function (copy_includes target source)
     execute_process(COMMAND cp ${source} ${CMAKE_BINARY_DIR}/utility/includes.h)
     target_include_directories(${target} PUBLIC ${CMAKE_BINARY_DIR})
